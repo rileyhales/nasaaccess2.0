5db8adaeb41d0833e7b1b71042af88bc3188a61e
Spencer McDonald <spencermcdonald11@gmail.com>
2018-10-01 14:38:27 +0700

    added geoserver to config.py

--- tethysapp-nasaaccess2/tethysapp/nasaaccess2/config.py
+++ tethysapp-nasaaccess2/tethysapp/nasaaccess2/config.py
@@ -3,3 +3,10 @@ import os
 data_path = os.path.join('/home/ubuntu/nasaaccess_data/')
 
 temp_path = os.path.join('/home/ubuntu/tethys_temp/nasaaccess/')
+
+geoserver = {'rest_url':'http://216.218.240.206:8080/geoserver/rest/',
+             'wms_url':'http://216.218.240.206:8080/geoserver/wms/',
+             'wfs_url':'http://216.218.240.206:8080/geoserver/wfs/',
+             'user':'admin',
+             'password':'geoserver',
+             'workspace':'nasaaccess'}
