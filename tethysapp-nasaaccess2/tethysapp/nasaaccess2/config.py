a2647a74ba4fdca3c22f3d30fa4978ef55391fca
Spencer McDonald <spencermcdonald11@gmail.com>
2018-11-09 15:51:32 +0700

    Update config.py

--- tethysapp-nasaaccess2/tethysapp/nasaaccess2/config.py
+++ tethysapp-nasaaccess2/tethysapp/nasaaccess2/config.py
@@ -1,9 +1,9 @@
 import os
 from .app import nasaaccess2
 
-data_path = os.path.join('home/ubuntu/nasaaccess_data')
+data_path = os.path.join('/home/ubuntu/nasaaccess_data/')
 
-temp_path = os.path.join('home/ubuntu/tethys_temp/nasaaccess')
+temp_path = os.path.join(nasaaccess2.get_app_workspace().path, temp)
 
 geoserver = {'rest_url':'http://216.218.240.206:8080/geoserver/rest/',
              'wms_url':'http://216.218.240.206:8080/geoserver/wms/',
@@ -12,4 +12,10 @@ geoserver = {'rest_url':'http://216.218.240.206:8080/geoserver/rest/',
              'password':'geoserver',
              'workspace':'nasaaccess',
              'URI': 'nasaaccess'}
+
+nasaaccess_py3 = os.path.join('/home/ubuntu/tethys/miniconda/envs/nasaaccess/bin/python3')
+
+nasaaccess_script = os.path.join('/home/ubuntu/subprocesses/nasaaccess.py')
+
+nasaaccess_log = os.path.join('/home/ubuntu/subprocesses/nasaaccess.log')
 
